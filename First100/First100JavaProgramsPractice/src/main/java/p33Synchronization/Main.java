7475773abb6c9fd61a4571367b2e37d0c8c1a97c
umalavanya <umalavanya7@gmail.com>
2026-01-14 12:36:27 -0500

    Synchroniation

---
+++ First100/First100JavaProgramsPractice/src/main/java/p33Synchronization/Main.java
@@ -0,0 +1,28 @@
+package p33Synchronization;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+         Counter counter = new Counter() ;
+
+         Thread t1 = new Thread(()  -> {
+
+             for(int i = 0 ; i<1000; i++){
+                 counter.increment() ;
+             }
+         });
+         Thread t2 = new Thread(() -> {
+             for(int i = 0 ; i<1000 ; i++) {
+                 counter.increment() ;
+             }
+         }) ;
+
+         t1.start() ;
+         t2.start() ;
+         t1.join() ;
+         t2.join() ;
+
+         System.out.println("Final count: "+ counter.getCount());
+
+    }
+}
