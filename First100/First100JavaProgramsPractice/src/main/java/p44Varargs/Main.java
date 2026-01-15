e48673e126e4b98b78b355c1665724cd3c154dd2
umalavanya <umalavanya7@gmail.com>
2026-01-15 08:55:00 -0500

    VarArgs

--- First100/First100JavaProgramsPractice/src/main/java/p44Varargs/Main.java
+++ First100/First100JavaProgramsPractice/src/main/java/p44Varargs/Main.java
@@ -3,6 +3,10 @@ package p44Varargs;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Sum (no args): " + sumAll());
+        System.out.println("Sum (3,5,7): " + sumAll(3,5,7));
+        System.out.println("Sum (1,2,3,4,5): " + sumAll(1,2,3,4,5));
+
     }
 
     public static int sumAll(int... numbers){
