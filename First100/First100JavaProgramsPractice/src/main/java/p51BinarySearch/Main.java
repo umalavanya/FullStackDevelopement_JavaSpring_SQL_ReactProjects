b120ffa58755b4b72b7b77d80ce971a7c2873ae6
umalavanya <umalavanya7@gmail.com>
2026-01-19 21:03:29 -0500

    binary search

---
+++ First100/First100JavaProgramsPractice/src/main/java/p51BinarySearch/Main.java
@@ -0,0 +1,26 @@
+package p51BinarySearch;
+public class Main {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int target = 23;
+        int index = binarySearch(sortedArray, target);
+        System.out.println("Target " + target + " found at index: " + index);
+    }
+}
