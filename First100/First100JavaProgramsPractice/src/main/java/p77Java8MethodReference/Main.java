8ec90b1bb4f892491218469ff8943f30fe4a158b
umalavanya <umalavanya7@gmail.com>
2026-01-28 15:45:57 -0500

    method reference

---
+++ First100/First100JavaProgramsPractice/src/main/java/p77Java8MethodReference/Main.java
@@ -0,0 +1,18 @@
+package p77Java8MethodReference;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        List<Person> people = names.stream()
+                .map(Person::new)
+                .collect(Collectors.toList());
+
+        people.forEach(System.out::println);
+    }
+    
+}
---
+++ First100/First100JavaProgramsPractice/src/main/java/p77Java8MethodReference/Person.java
@@ -0,0 +1,12 @@
+package p77Java8MethodReference;
+
+public class Person {
+    private String name ;
+    public Person(String name){
+        this.name = name ;
+    }
+    @Override
+    public String toString(){
+        return "Person{name = '"+name+"'}";
+    }
+}
