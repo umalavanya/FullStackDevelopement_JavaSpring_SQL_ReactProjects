2b972d8dc46c28e040803eab186fd51cb3adf5ea
umalavanya <umalavanya7@gmail.com>
2026-01-28 13:48:34 -0500

    map

--- First100/First100JavaProgramsPractice/src/main/java/p68Java8forEachOnMap/Main.java
+++ First100/First100JavaProgramsPractice/src/main/java/p68Java8forEachOnMap/Main.java
@@ -1,5 +1,17 @@
 package p68Java8forEachOnMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>() ;
+        map.put("Alice",90) ;
+        map.put("Bob", 85);
+        map.put("Charlie", 95);
+
+        map.forEach((key, value) -> System.out.println(key + ": " + value));
+
+    }
     
 }
