dc334eae279b762fedf3bd99490c6c31f2ea9a04
umalavanya <umalavanya7@gmail.com>
2026-01-29 09:20:30 -0500

    switch

---
+++ First100/First100JavaProgramsPractice/src/main/java/p87Switch/Main.java
@@ -0,0 +1,20 @@
+package p87Switch;
+
+public class Main {
+    public static void main(String[] args) {
+        int day = 3 ;
+
+        String dayName = switch(day){
+            case 1 -> "Monday" ;
+            case 2 -> "Tuesday" ;
+            case 3 -> "Wednesday" ;
+            case 4 -> "Thursday" ;
+            case 5 -> "Friday" ;
+            case 6 -> "Saturday" ;
+            case 7 -> "Sunday" ;
+            default -> "Invalid day" ;
+        } ;
+
+        System.out.println("Day"+day+" is "+dayName);
+    }
+}
