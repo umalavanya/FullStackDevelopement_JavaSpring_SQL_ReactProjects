aa636f1a85aa7f1a94af654cb3d9525bc29fa359
umalavanya <umalavanya7@gmail.com>
2026-03-05 11:22:09 -0500

    Update script_practice.js

--- First100/First100_JavaScript/ex2_simpleCalculator/script_practice.js
+++ First100/First100_JavaScript/ex2_simpleCalculator/script_practice.js
@@ -1,5 +1,20 @@
 (function () {
     // grab display element
     const display = document.getElementById('calc-display') ;
-    console.log(display);
+    
+    
+    //calculator state
+    let currentInput = '0';  // what's shown on display
+    let previousOperand = null;
+    let currentOperator = null ;
+    let shouldResetDisplay = false ;
+
+
+    // helper to update the input field
+    function updateDisplay(value) {
+        display.value = value.slice(0,12); //limit length to avoid overflow
+        
+    }
+
+
 })() ;
