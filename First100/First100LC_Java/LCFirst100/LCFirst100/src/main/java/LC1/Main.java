687846ca9cf3fd68ec9c0c4d9864bc4a840f8a30
umalavanya <umalavanya7@gmail.com>
2026-03-07 15:33:46 -0500

    leet code problems practice

--- First100/First100LC_Java/LCFirst100/LCFirst100/src/main/java/LC1/Main.java
+++ First100/First100LC_Java/LCFirst100/LCFirst100/src/main/java/LC1/Main.java
@@ -1,4 +1,26 @@
 package LC1;
 
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(totalMoney(10));
+
+    }
+
+
+    public static int totalMoney(int n) {
+        int sum = 0 ;
+        int day = 1 ;
+        int week = 1 ;
+        int rem ;
+        while(day<=n){
+            rem = (day%7) ;
+            sum += (rem==0?7:rem) + (week-1) ;
+            day++ ;
+            if (rem == 0){
+                week++ ;
+            }
+        }
+        return sum ;
+    }
 }
