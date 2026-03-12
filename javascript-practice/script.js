be4108d8b6895500a6e69eb7b8815b96ee655a8b
umalavanya <umalavanya7@gmail.com>
2026-03-12 11:56:38 -0400

    javascript practice

--- javascript-practice/script.js
+++ javascript-practice/script.js
@@ -1,1 +1,10 @@
-document.writeln("<h1 style:>Hello</h1>")
+function test() {
+	     if (1) {
+	        let m = 70;
+			// let m = 80; // redeclaration with let keyword is not	possible
+			var n = 80;
+			var n = 90; // redeclaration with var keyword is possible
+			document.writeln("m = " + m + ", n = " + n);
+		 }
+	  }
+      test();
