a07130eca7068cea23092026b5675f5802ead7a5
umalavanya <umalavanya7@gmail.com>
2026-03-13 10:22:35 -0400

    practicing javascript Brocode

---
+++ javascript-practice/javascript-brocode/js1-starting/index.js
@@ -0,0 +1,20 @@
+// console.log(`Hello`) ; // `` - back ticks are called template literals 
+// console.log(`I like piza`) ;
+
+// window.alert(`This is an alert` ) ;
+// window.alert(`I like a pizza` ) ;
+
+
+document.getElementById("myH1").textContent = `Hello` ;
+document.getElementById("myP").textContent = `I like piza!`
+
+// This is a comment
+
+/* 
+Multi line comment
+This 
+is
+a 
+comment
+*/
+
