b49d3edcec77fd2d7fd1d46cf00b48ada2d4db6a
umalavanya <umalavanya7@gmail.com>
2026-03-13 10:48:43 -0400

    operators

---
+++ javascript-practice/javascript-brocode/js2-variables/index.js
@@ -0,0 +1,38 @@
+//variable = A container that stores a value .
+//           Behaves as if it were the value it contains
+
+
+//1.declaration  let x ;
+//2. assignment  x = 100 ;
+
+let x ;
+x = 123 ;
+
+
+
+let age = 25 ;
+let price = 10.99 ;
+let gpa = 2.1 ;
+let isStudent = false ;
+
+
+console.log(x) ;
+console.log(age) ;
+console.log(price) ;
+console.log(gpa) ;
+
+
+console.log(`You are ${age} years old`) ;
+console.log(``)
+
+
+
+//**************************** */
+
+let fullName= "Uma Lavanya Chintapanti";
+
+console.log(typeof firstname);
+
+document.getElementById("p1").textContent = fullName ;
+document.getElementById("p2").textContent = age ;
+document.getElementById("p3").textContent = isStudent ;
---
+++ javascript-practice/javascript-brocode/js3-operators/index.js
@@ -0,0 +1,12 @@
+let students = 30 ;
+
+students = students + 1 ;
+
+
+students = students - 1 ;
+
+students = students*2 ;
+
+students = students/4 ;
+
+console.log(students) ;
