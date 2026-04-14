32c90d6687bc546c85237b5241346022efa78fe2
umalavanya <umalavanya7@gmail.com>
2026-04-13 23:45:42 -0400

    Update server.js

--- Ajax-practice/ajax-practice-5/server.js
+++ Ajax-practice/ajax-practice-5/server.js
@@ -5,3 +5,13 @@ const app = express() ;
 
 app.use(express.json()) ;
 app.use(cors()) ;
+
+
+app.get("/products",(req,res) =>{
+    res.json({message:"The products are here!!"})
+})
+
+const PORT = 3000 ;
+app.listen(PORT, () => {
+    console.log(`The server is running on port number: ${PORT}`) ;
+}) ;
