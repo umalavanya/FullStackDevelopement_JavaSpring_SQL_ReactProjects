649d348d8c54835c0135be4beca728094ae6bea1
umalavanya <umalavanya7@gmail.com>
2026-05-21 05:18:51 -0400

    clearTimeout practicing

---
+++ Node-Practice/set1/node12_clearTimeout.js
@@ -0,0 +1,7 @@
+// Set a timeout and store its Id
+const timeoutId = setTimeout(() => {
+    console.log("This will not run") ;
+}, 3000) ;
+
+// Cancel the timeout before it executes
+clearTimeout(timeoutId) ;
