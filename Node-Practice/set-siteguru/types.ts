675892872b69272bcafd5fa6f413215f2a4829dc
umalavanya <umalavanya7@gmail.com>
2026-06-03 13:38:32 -0400

    Update types.ts

--- Node-Practice/set-siteguru/types.ts
+++ Node-Practice/set-siteguru/types.ts
@@ -7,3 +7,8 @@ let u:any = true ;
 u = "string" ; // Error 
 Math.round(u) ;
 console.log(u, Math.round(u))
+
+// difference between the any and unknown
+// unknown must be type-checked before use
+// You cant access properties on an unknown type without type assertion
+// You can't call or construct values of type unknown
